97725c0ad115094d7f505b584f67602767a1fcc2
Koby Lewis <kobyjlewis@gmail.com>
2021-10-26 20:24:20 -0600

    add start of AddEventScreen.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AddEventScreen } from './AddEventScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>This will be our Dev Club Free Food Application!</Text>
+      <AddEventScreen/>
       <StatusBar style="auto" />
     </View>
   );
